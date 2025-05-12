645c5e3e82fc9c57902a359561c67a1b9e60b2ba
bhearley <109361610+bhearley@users.noreply.github.com>
2025-05-12 15:19:57 -0400

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='GRCMI',           
+    version='0.1.0',               
+    description='Short description of your package',
+    author='Brandon Hearley',
+    author_email='brandon.l.hearley@nasa.gov',
+    url='https://github.com/bhearley/GRC_GrantaMI',
+    packages=find_packages(),         # Automatically finds all packages (folders with __init__.py)
+    install_requires=[],              # List any runtime dependencies here (e.g., ['requests'])
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.7',          # Adjust for your Python version compatibility
+)
